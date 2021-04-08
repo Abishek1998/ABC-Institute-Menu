302b8eee96a9679e37bd2e5575840f2003fc3eaf
it18231960@my.sliit.lk <it18231960@my.sliit.lk>
2021-04-08 11:08:47 +0530

    Lecture comit2

--- ABC Institute Menu/addLecturer.Designer.cs
+++ ABC Institute Menu/addLecturer.Designer.cs
@@ -40,7 +40,7 @@ namespace ABC_Institute_Menu
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(552, 325);
+            this.button2.Location = new System.Drawing.Point(553, 422);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(120, 40);
             this.button2.TabIndex = 27;
@@ -54,7 +54,7 @@ namespace ABC_Institute_Menu
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(412, 325);
+            this.button1.Location = new System.Drawing.Point(413, 422);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(120, 40);
             this.button1.TabIndex = 26;
