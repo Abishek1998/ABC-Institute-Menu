e21b42bc87d37ec5833eb693b180b4dfa94dea2a
Abishek1998 <abishaj1998@gmail.com>
2021-04-08 23:21:38 +0530

    safcsafsaf

--- ABC Institute Menu/addWorkingDays.cs
+++ ABC Institute Menu/addWorkingDays.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace ABC_Institute_Menu
 {
@@ -17,9 +18,55 @@ namespace ABC_Institute_Menu
             InitializeComponent();
         }
 
+        private SQLiteConnection sql_con;
+        private SQLiteCommand sql_cmd;
+        private SQLiteDataAdapter DB;
+        private DataSet DS = new DataSet();
+        private DataTable DT = new DataTable();
+
+        //set Connection
+        private void SetConnection()
+        {
+            sql_con = new SQLiteConnection("Data Source=timetable.db;version=3;New=False;Compress=True");
+        }
+
+        //set execute query
+        private void ExecuteQuery(String txtQuery)
+        {
+            SetConnection();
+            sql_con.Open();
+            sql_cmd = sql_con.CreateCommand();
+            sql_cmd.CommandText = txtQuery;
+            sql_cmd.ExecuteNonQuery();
+            sql_con.Close();
+        }
+
+        //set LoadDb
+        private void LoadData()
+        {
+            SetConnection();
+            sql_con.Open();
+            sql_cmd = sql_con.CreateCommand();
+            string CommandText = "select * from workingDaysHours";
+            DB = new SQLiteDataAdapter(CommandText, sql_con);
+            DS.Reset();
+            DB.Fill(DS);
+            DT = DS.Tables[0];
+            dataGridView2.DataSource = DT;
+            sql_con.Close();
+        }
+
+        //ADD
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string txtQuery = "Insert into workingDaysHours (noOfWorkingDays, hours, minutes) values ('" + numericUpDown1.Text + "' , '" + numericUpDown2.Text + "' , '" + numericUpDown3.Text + "' )";
+            ExecuteQuery(txtQuery);
+            LoadData();
+        }
+
         private void Form2_Load(object sender, EventArgs e)
         {
-
+            LoadData();
         }
 
         private void label1_Click(object sender, EventArgs e)
