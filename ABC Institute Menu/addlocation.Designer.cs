c12062a81e092e207b3abc2109460b37b3db42ca
SANDINIJAY <sandinijay@gmail.com>
2021-05-13 18:57:52 +0530

    ewfwefw

--- ABC Institute Menu/addlocation.Designer.cs
+++ ABC Institute Menu/addlocation.Designer.cs
@@ -33,8 +33,7 @@ namespace ABC_Institute_Menu
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.rbLab = new System.Windows.Forms.RadioButton();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.noOfWorkingDays = new System.Windows.Forms.Label();
@@ -42,6 +41,12 @@ namespace ABC_Institute_Menu
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.LectureHall = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -60,12 +65,12 @@ namespace ABC_Institute_Menu
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(578, 645);
+            this.button2.Location = new System.Drawing.Point(1133, 470);
             this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(160, 49);
             this.button2.TabIndex = 25;
-            this.button2.Text = "Clear All";
+            this.button2.Text = "Delete Record";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -76,49 +81,39 @@ namespace ABC_Institute_Menu
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(392, 645);
+            this.button1.Location = new System.Drawing.Point(767, 470);
             this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(160, 49);
             this.button1.TabIndex = 24;
-            this.button1.Text = "Save Changes";
+            this.button1.Text = "Add";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.textBox1.Location = new System.Drawing.Point(438, 219);
+            this.textBox1.Location = new System.Drawing.Point(305, 315);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(300, 30);
             this.textBox1.TabIndex = 27;
             // 
-            // radioButton1
+            // rbLab
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(438, 425);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(105, 21);
-            this.radioButton1.TabIndex = 32;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Lecture Hall";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(633, 425);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(53, 21);
-            this.radioButton2.TabIndex = 33;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Lab";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.rbLab.AutoSize = true;
+            this.rbLab.Location = new System.Drawing.Point(1240, 213);
+            this.rbLab.Name = "rbLab";
+            this.rbLab.Size = new System.Drawing.Size(53, 21);
+            this.rbLab.TabIndex = 33;
+            this.rbLab.TabStop = true;
+            this.rbLab.Text = "Lab";
+            this.rbLab.UseVisualStyleBackColor = true;
+            this.rbLab.CheckedChanged += new System.EventHandler(this.rbLab_CheckedChanged);
             // 
             // textBox3
             // 
             this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.textBox3.Location = new System.Drawing.Point(438, 313);
+            this.textBox3.Location = new System.Drawing.Point(305, 413);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(300, 30);
             this.textBox3.TabIndex = 34;
@@ -126,16 +121,17 @@ namespace ABC_Institute_Menu
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.textBox2.Location = new System.Drawing.Point(438, 511);
+            this.textBox2.Location = new System.Drawing.Point(993, 306);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(300, 30);
             this.textBox2.TabIndex = 36;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // noOfWorkingDays
             // 
             this.noOfWorkingDays.AutoSize = true;
             this.noOfWorkingDays.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.noOfWorkingDays.Location = new System.Drawing.Point(56, 224);
+            this.noOfWorkingDays.Location = new System.Drawing.Point(60, 320);
             this.noOfWorkingDays.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.noOfWorkingDays.Name = "noOfWorkingDays";
             this.noOfWorkingDays.Size = new System.Drawing.Size(151, 25);
@@ -157,7 +153,7 @@ namespace ABC_Institute_Menu
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(56, 318);
+            this.label2.Location = new System.Drawing.Point(60, 418);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(129, 25);
@@ -168,7 +164,7 @@ namespace ABC_Institute_Menu
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(56, 420);
+            this.label3.Location = new System.Drawing.Point(748, 213);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(179, 25);
@@ -179,12 +175,74 @@ namespace ABC_Institute_Menu
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(56, 516);
+            this.label4.Location = new System.Drawing.Point(748, 309);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(97, 25);
             this.label4.TabIndex = 41;
             this.label4.Text = "Capacity";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.RoyalBlue;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(951, 470);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(160, 49);
+            this.button3.TabIndex = 42;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.Location = new System.Drawing.Point(117, 598);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1176, 293);
+            this.dataGridView1.TabIndex = 43;
+            this.dataGridView1.AutoSizeColumnModeChanged += new System.Windows.Forms.DataGridViewAutoSizeColumnModeEventHandler(this.Fill);
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(60, 224);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 25);
+            this.label5.TabIndex = 45;
+            this.label5.Text = "ID";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox4.Location = new System.Drawing.Point(305, 219);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(300, 30);
+            this.textBox4.TabIndex = 44;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // LectureHall
+            // 
+            this.LectureHall.AutoSize = true;
+            this.LectureHall.Location = new System.Drawing.Point(993, 213);
+            this.LectureHall.Name = "LectureHall";
+            this.LectureHall.Size = new System.Drawing.Size(105, 21);
+            this.LectureHall.TabIndex = 46;
+            this.LectureHall.TabStop = true;
+            this.LectureHall.Text = "Lecture Hall";
+            this.LectureHall.UseVisualStyleBackColor = true;
             // 
             // addlocation
             // 
@@ -192,6 +250,11 @@ namespace ABC_Institute_Menu
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1445, 849);
+            this.Controls.Add(this.LectureHall);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -199,14 +262,15 @@ namespace ABC_Institute_Menu
             this.Controls.Add(this.noOfWorkingDays);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.rbLab);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "addlocation";
             this.Text = "addlocation";
+            this.Load += new System.EventHandler(this.addlocation_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -217,8 +281,7 @@ namespace ABC_Institute_Menu
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton rbLab;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label noOfWorkingDays;
@@ -226,5 +289,10 @@ namespace ABC_Institute_Menu
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.RadioButton LectureHall;
     }
 }
---
+++ ABC Institute Menu/addsession.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace ABC_Institute_Menu
+{
+    public partial class addsession : Form
+    {
+        string Tag  ;
+
+        public addsession()
+        {
+            InitializeComponent();
+        }
+
+        private SQLiteConnection sql_con;
+        private SQLiteCommand sql_cmd;
+        private SQLiteDataAdapter DB;
+        private DataSet DS = new DataSet();
+        private DataTable DT = new DataTable();
+
+        //set Connection
+        private void SetConnection()
+        {
+            sql_con = new SQLiteConnection("Data Source=timetable.db;version=3;New=False;Compress=True");
+        }
+
+        //set execute query
+        private void ExecuteQuery(String txtQuery)
+        {
+            SetConnection();
+            sql_con.Open();
+            sql_cmd = sql_con.CreateCommand();
+            sql_cmd.CommandText = txtQuery;
+            sql_cmd.ExecuteNonQuery();
+            sql_con.Close();
+        }
+
+        //set LoadDb
+        private void LoadData()
+        {
+            SetConnection();
+            sql_con.Open();
+            sql_cmd = sql_con.CreateCommand();
+            string CommandText = "select * from sessionDetails";
+            DB = new SQLiteDataAdapter(CommandText, sql_con);
+            DS.Reset();
+            DB.Fill(DS);
+            DT = DS.Tables[0];
+            dataGridView1.DataSource = DT;
+            sql_con.Close();
+        }
+
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addsession_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LectureHall_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rbLab_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //ADD
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (rbLectures.Checked)
+            {
+                Tag = "Lecture";
+            }
+
+            if (rbLabs.Checked)
+            {
+                Tag = "Lab";
+            }
+
+            if (rbTutes.Checked)
+            {
+                Tag = "Tute";
+            }
+
+
+            string txtQuery = "Insert into sessionDetails (ID,Lecturer1, Lecturer2 , SubCode , Tag , GroupID , SubGroupID , Building , Room ) values ('" + textBox4.Text + "','" + textBox2.Text + "' , '" + textBox3.Text + "' , '" + textBox5.Text + "', '" + Tag + "', '" + textBox1.Text + "', '" + textBox6.Text + "', '" + textBox7.Text + "', '" + textBox8.Text + "' )";
+
+
+
+            ExecuteQuery(txtQuery);
+            LoadData();
+
+            MessageBox.Show("One Record Added Successfully !! ");
+            return;
+
+        }
+
+        //Delete
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            DialogResult dialogResult = MessageBox.Show("Are you sure do you want to delete this record?", "Delete Action", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                //do something
+                string txtQuery = "delete from sessionDetails where ID = '" + textBox4.Text + "'";
+                ExecuteQuery(txtQuery);
+                LoadData();
+                MessageBox.Show("Record Deleted !! ");
+                return;
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+                //do something else
+                return;
+            }
+        }
+
+        //Update
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            if (rbLectures.Checked)
+            {
+                Tag = "Lecture";
+            }
+
+            if (rbLabs.Checked)
+            {
+                Tag = "Lab";
+            }
+
+            if (rbTutes.Checked)
+            {
+                Tag = "Tute";
+            }
+
+
+            string txtQuery = "update sessionDetails set Lecturer1 = '" + textBox2.Text + "' , Lecturer2 = '" + textBox3.Text + "',  SubCode = '" + textBox5.Text + "', Tag = '" + Tag + "', GroupID = '" + textBox1.Text + "', SubGroupID = '" + textBox6.Text + "', Building = '" + textBox7.Text + "', Room = '" + textBox8.Text + "'  where ID = '" + textBox4.Text + "' ";
+
+            ExecuteQuery(txtQuery);
+            LoadData();
+            MessageBox.Show("Updated Successfully !! ");
+            return;
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBox1.Text = dataGridView1.SelectedRows[0].Cells[5].Value.ToString();
+            textBox2.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+            textBox3.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+            textBox4.Text = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+            textBox5.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+            textBox6.Text = dataGridView1.SelectedRows[0].Cells[6].Value.ToString();
+            textBox7.Text = dataGridView1.SelectedRows[0].Cells[7].Value.ToString();
+            textBox8.Text = dataGridView1.SelectedRows[0].Cells[8].Value.ToString();
+
+
+
+
+            if (dataGridView1.SelectedRows[0].Cells[4].Value.ToString() == "Lab")
+            {
+                rbLabs.Checked = true;
+            }
+
+            else if (dataGridView1.SelectedRows[0].Cells[4].Value.ToString() == "Lecture")
+            {
+                rbLectures.Checked = true;
+            }
+            else if (dataGridView1.SelectedRows[0].Cells[4].Value.ToString() == "Lecture")
+            {
+                rbTutes.Checked = true;
+            }
+
+            else
+            {
+                rbLabs.Checked = false;
+                rbLectures.Checked = false;
+                rbTutes.Checked = false;
+            }
+
+
+        }
+    }
+}
